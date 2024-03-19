8807d9f569259d9f0ac08cc764d817a5f33eff8d
mpolak00 <115738574+mpolak00@users.noreply.github.com>
2024-03-19 18:10:50 +0100

    Update Program.cs

--- PIS3124234/Program.cs
+++ PIS3124234/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Novo");
-Console.WriteLine("Editanooooo");
+Console.WriteLine("konflikt");
 
